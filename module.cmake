044a55eb00ffe4653f31941f6c58bc20872c4279
David Gobbi <david.gobbi@gmail.com>
2015-10-15 07:16:22 -0600

    Tighten up the remote VTK module build.
    Much of the CMake logic for the stand-alone build can be skipped
    for the remote module build.

--- module.cmake
+++ module.cmake
@@ -1,17 +1,21 @@
-set(DOCUMENTATION "This package contains a set of classes for managing DICOM
+set(DOCUMENTATION
+"This package contains a set of classes for managing DICOM
 files and metadata from within VTK, and some utility programs
-for interrogating and converting DICOM files.")
+for interrogating and converting DICOM files."
+)
 
-vtk_module( vtkDICOM
+vtk_module(vtkDICOM
   DESCRIPTION
     "${DOCUMENTATION}"
   DEPENDS
-  vtkIOImage
-  vtkIOSQL
-  vtkImagingStatistics
-  vtkInteractionStyle
-  vtkRenderingImage
-  vtkRenderingOpenGL
-  vtkzlib
-  vtkWrappingPythonCore
+    vtkIOImage
+    vtkIOSQL
+    vtkImagingStatistics
+    vtkInteractionStyle
+    vtkRenderingImage
+    vtkRenderingOpenGL
+    vtkWrappingPythonCore
+  PRIVATE_DEPENDS
+    vtksys
+    vtkzlib
 )
